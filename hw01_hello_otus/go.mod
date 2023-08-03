307e2aa65828953a3a9a3c6f899edd8329156330
Sergey Makarichev <harleymack@mail.ru>
2023-08-03 19:05:51 +0300

    HW1 is completed

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/otus_golang_hw/hw01_hello_otus
 
 go 1.19
+
+require golang.org/x/example/hello v0.0.0-20230802215733-17e2836bb62e
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	const greeting = "Hello world"
+	fmt.Println(reverse.String(greeting))
 }
