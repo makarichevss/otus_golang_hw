9505aa5f11f0981b4e96af13667dec4c40dac1b1
Sergey Makarichev <harleymack@mail.ru>
2023-08-08 21:59:53 +0300

    experimenting

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,7 +11,7 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(str string) (string, error) {
 	var builder strings.Builder
 
-	// Кидает ошибку, если строка начинается с цифры или содержит числа больше 9
+	// Кидает ошибку, если строка начинается с цифры или содержит числа больше 9.
 	if len(str) > 0 && (unicode.IsDigit(rune(str[0])) || hasConsecutiveDigits(str)) {
 		return "", ErrInvalidString
 	}
@@ -20,12 +20,12 @@ func Unpack(str string) (string, error) {
 		r := rune(str[i])
 		i++
 
-		// Если следующая руна является цифрой, она используется как множитель
+		// Если следующая руна является цифрой, она используется как множитель.
 		if i < len(str) && unicode.IsDigit(rune(str[i])) {
-			// Преобразовать байт цифры в int
+			// Преобразовать байт цифры в int.
 			count := int(str[i] - '0')
 			builder.WriteString(strings.Repeat(string(r), count))
-			// пропустить цифру
+			// пропустить цифру.
 			i++
 		} else {
 			builder.WriteRune(r)
@@ -35,7 +35,7 @@ func Unpack(str string) (string, error) {
 	return builder.String(), nil
 }
 
-// Проверяет, есть ли в строке две или более подряд идущие цифры
+// Проверяет, есть ли в строке две или более подряд идущие цифры.
 func hasConsecutiveDigits(s string) bool {
 	for i := 0; i < len(s)-1; i++ {
 		if unicode.IsDigit(rune(s[i])) && unicode.IsDigit(rune(s[i+1])) {
