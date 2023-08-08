a4a31b06cac9adff4d26d95203ebaeb9efa5b0ff
Sergey Makarichev <harleymack@mail.ru>
2023-08-08 21:55:17 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+
+	// Кидает ошибку, если строка начинается с цифры или содержит числа больше 9
+	if len(str) > 0 && (unicode.IsDigit(rune(str[0])) || hasConsecutiveDigits(str)) {
+		return "", ErrInvalidString
+	}
+
+	for i := 0; i < len(str); {
+		r := rune(str[i])
+		i++
+
+		// Если следующая руна является цифрой, она используется как множитель
+		if i < len(str) && unicode.IsDigit(rune(str[i])) {
+			count := int(str[i] - '0') // Преобразовать байт цифры в int
+			builder.WriteString(strings.Repeat(string(r), count))
+			i++ // пропустить цифру
+		} else {
+			builder.WriteRune(r)
+		}
+	}
+
+	return builder.String(), nil
+}
+
+// Проверяет, есть ли в строке две или более подряд идущие цифры
+func hasConsecutiveDigits(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		if unicode.IsDigit(rune(s[i])) && unicode.IsDigit(rune(s[i+1])) {
+			return true
+		}
+	}
+
+	return false
 }
