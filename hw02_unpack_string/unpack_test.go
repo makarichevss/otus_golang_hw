24731636fc6eea832eaf892db1dd02b283c86f72
Sergey Makarichev <harleymack@mail.ru>
2023-08-09 19:41:12 +0300

    add 4 tests

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "x0", expected: ""},
+		{input: "x1", expected: "x"},
+		{input: "x3y4", expected: "xxxyyyy"},
+		{input: "xyz", expected: "xyz"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
